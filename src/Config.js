92e581cbd711ac28489def25ab6264008b4c1169
nickly24 <nikolaypribysh@mail.ru>
2025-10-01 17:05:45 +0300

    fix link

--- src/Config.js
+++ src/Config.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL = 'https://nickly24-cpm-serv-f15d.twc1.net';
-export const API_EXAM_URL = 'http://127.0.0.1:81';
+export const API_EXAM_URL = 'https://nickly24-cpm-exam-main-225a.twc1.net/';
