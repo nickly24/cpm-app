00266267eb4e4b9f56e252c937412b1d03ffa711
nickly24 <nikolaypribysh@mail.ru>
2025-12-19 15:59:02 +0300

    links

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,4 @@
 // Прокси-сервер (единая точка доступа)
-export const API_BASE_URL = 'https://nickly24-cpm-proxy-467b.twc1.net/';
-// Для обратной совместимости - все запросы идут через прокси
+export const API_BASE_URL = 'https://nickly24-cpm-proxy-58b0.twc1.net';
+// Для обратной совместимости - все запросы идут через проксиdd
 export const API_EXAM_URL = API_BASE_URL;
